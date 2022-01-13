3937a4a77a5606ab22dc3838906c42a170c819d0
gorobot-nz <gorobot.nz@gmail.com>
2022-01-13 20:35:47 +0300

    Add start func

--- saver/cmd/main.go
+++ saver/cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	log "github.com/sirupsen/logrus"
+	"saver/wrappers"
+)
 
+func main() {
+	cfg := wrappers.Config{}
+
+	es := wrappers.NewElasticWrapper(cfg)
+	rabbit := wrappers.NewRabbitMQSaverWrapper("amqp://guest:guest@localhost:5672/", es)
+	rabbit.Listen()
+
+	forever := make(chan bool)
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
