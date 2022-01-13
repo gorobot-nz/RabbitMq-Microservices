0023f9d0ad3301f4b8f6f09100073e403658f341
gorobot-nz <gorobot.nz@gmail.com>
2022-01-13 16:15:40 +0300

    Collect all links from url

--- collector/cmd/main.go
+++ collector/cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/gocolly/colly/v2"
+)
 
+func main() {
+	c := colly.NewCollector()
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Println(e.Request.AbsoluteURL(link))
+	})
+
+	err := c.Visit("https://go.dev/learn/")
+	if err != nil {
+		return
+	}
 }
