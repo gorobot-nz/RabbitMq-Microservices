8ca979a89428080661ca2d69a292efc339a78532
gorobot-nz <gorobot.nz@gmail.com>
2022-01-13 17:38:16 +0300

    Fix

--- collector/cmd/main.go
+++ collector/cmd/main.go
@@ -2,14 +2,7 @@ package main
 
 import (
 	"collector/wrappers"
-	"log"
 )
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
 
 func main() {
 	rabbitmq := wrappers.NewRabbitMQWrapper("amqp://guest:guest@localhost:5672/")
