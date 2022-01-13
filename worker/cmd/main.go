15353b85c54e272562d5080ab551026ccfdf9c8d
gorobot-nz <gorobot.nz@gmail.com>
2022-01-13 18:35:26 +0300

    Add infinite work

--- worker/cmd/main.go
+++ worker/cmd/main.go
@@ -6,4 +6,6 @@ func main() {
 	rabbitmq := wrappers.NewRabbitWorkerMQWrapper("amqp://guest:guest@localhost:5672/")
 	worker := wrappers.NewWorkerWrapper(rabbitmq)
 	worker.Visit("https://go.dev/learn/")
+	forever := make(chan bool)
+	<-forever
 }
