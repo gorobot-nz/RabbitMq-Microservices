e058a642f1012fd909be65e9df4d1de35332cc05
gorobot-nz <gorobot.nz@gmail.com>
2022-01-13 19:09:28 +0300

    Refactor

--- collector/wrappers/collector_wrapper.go
+++ collector/wrappers/collector_wrapper.go
@@ -7,20 +7,21 @@ import (
 
 type CollectorWrapper struct {
 	collector *colly.Collector
+	rmq       *RabbitMQCollectorWrapper
 }
 
 func NewCollectorWrapper(rmq *RabbitMQCollectorWrapper) *CollectorWrapper {
 	collector := colly.NewCollector()
-	collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		log.Infof("Find link %s", e.Request.AbsoluteURL(link))
-		rmq.Send(e.Request.AbsoluteURL(link))
-	})
 
-	return &CollectorWrapper{collector}
+	return &CollectorWrapper{collector, rmq}
 }
 
 func (w *CollectorWrapper) Run(url string) error {
+	w.collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		log.Infof("Find link %s", e.Request.AbsoluteURL(link))
+		w.rmq.Send(e.Request.AbsoluteURL(link))
+	})
 	err := w.collector.Visit(url)
 	if err != nil {
 		return err
